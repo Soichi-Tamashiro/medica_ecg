298f3fa2c1482cdf5451ec1fb2c2607cfc3dc213
Soichi Tamashiro <wyvernys@live.com>
2019-08-27 13:44:48 -0500

    Changes to be committed: 	new file:   index.h modified:   medica_ecg.ino

--- medica_ecg.ino
+++ medica_ecg.ino
@@ -1,20 +1,72 @@
-const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
+/*
+ * ESP8266 NodeMCU Real Time Graphs Demo
+ * Updates and Gets data from webpage without page refresh
+ * https://circuits4you.com
+ */
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 
-int sensorValue = 0;  // value read from the pot
-int outputValue = 0;  // value to output to a PWM pin
+#include "index.h" //Our HTML webpage contents with javascripts
 
-void setup() {
-  // initialize serial communication at 115200
-  Serial.begin(115200);
+#define LED 2  //On board LED
+
+//SSID and Password of your WiFi router
+const char* ssid = "My Phone HTC";
+const char* password = "N1o2w3a4r5n6i7n8g9";
+
+ESP8266WebServer server(80); //Server on port 80
+
+//===============================================================
+// This routine is executed when you open its IP in browser
+//===============================================================
+void handleRoot() {
+ String s = MAIN_page; //Read HTML contents
+ server.send(200, "text/html", s); //Send web page
 }
 
-void loop() {
-  // read the analog in value
-  sensorValue = analogRead(analogInPin);
-  
-  // read the value from the sensor:
-  float voltage = (sensorValue * 3.3) / 1024.0;// ADC of 10 bits
+void handleADC() {
+ int sensorValue = analogRead(A0);
+ float voltage = (sensorValue * 3.3) / 1024.0;// ADC of 10 bits
   float mV = voltage*1000;
-  Serial.println(mV);
-  delay(1000);
+ String adcValue = String(mV);
+ digitalWrite(LED,!digitalRead(LED)); //Toggle LED on data request ajax
+ server.send(200, "text/plane", adcValue); //Send ADC value only to client ajax request
+}
+//==============================================================
+//                  SETUP
+//==============================================================
+void setup(void){
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
+  Serial.println("");
+
+  //Onboard LED port Direction output
+  pinMode(LED,OUTPUT); 
+  
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+ 
+  server.on("/", handleRoot);      //Which routine to handle at root location. This is display page
+  server.on("/readADC", handleADC); //This page is called by java Script AJAX
+
+  server.begin();                  //Start server
+  Serial.println("HTTP server started");
+}
+//==============================================================
+//                     LOOP
+//==============================================================
+void loop(void){
+  server.handleClient();          //Handle client requests
 }
