2a48279c0a08eb726c00d35b1f52a8619d5fb9e3
Soichi <wyvernys@live.com>
2019-11-26 01:51:18 -0500

    Changes to be committed: 	new file:   medica_ecg_thingspealk_bpm/medica_ecg_thingspealk_bpm.ino 	new file:   medica_ecg_thingspealk_bpm/secrets.h

---
+++ medica_ecg_thingspealk_bpm/medica_ecg_thingspealk_bpm.ino
@@ -0,0 +1,151 @@
+/*
+  WriteMultipleFields
+  
+  Description: Writes values to fields 1,2,3,4 and status in a single ThingSpeak update every 20 seconds.
+  
+  Hardware: ESP8266 based boards
+  
+  !!! IMPORTANT - Modify the secrets.h file for this project with your network connection and ThingSpeak channel details. !!!
+  
+  Note:
+  - Requires ESP8266WiFi library and ESP8622 board add-on. See https://github.com/esp8266/Arduino for details.
+  - Select the target hardware from the Tools->Board menu
+  - This example is written for a network using WPA encryption. For WEP or WPA, change the WiFi.begin() call accordingly.
+  
+  ThingSpeak ( https://www.thingspeak.com ) is an analytic IoT platform service that allows you to aggregate, visualize, and 
+  analyze live data streams in the cloud. Visit https://www.thingspeak.com to sign up for a free account and create a channel.  
+  
+  Documentation for the ThingSpeak Communication Library for Arduino is in the README.md folder where the library was installed.
+  See https://www.mathworks.com/help/thingspeak/index.html for the full ThingSpeak documentation.
+  
+  For licensing information, see the accompanying license file.
+  
+  Copyright 2018, The MathWorks, Inc.
+*/
+
+#include "ThingSpeak.h"
+#include "secrets.h"
+#include <ESP8266WiFi.h>
+
+char ssid[] = SECRET_SSID;   // your network SSID (name) 
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+WiFiClient  client;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+//===============================================================
+// Initialize BPM values
+//===============================================================
+int UpperThreshold = 518;
+int LowerThreshold = 490;
+int reading = 0;
+int bpm_prom=0;
+float BPM = 0.0;
+float BPM_SUM =0.0;
+bool IgnoreReading = false;
+bool FirstPulseDetected = false;
+unsigned long FirstPulseTime = 0;
+unsigned long SecondPulseTime = 0;
+unsigned long PulseInterval = 0;
+//===============================================================
+// Initialize our values
+//===============================================================
+String myStatus = "";
+//===============================================================
+//                        SETUP
+//===============================================================
+void setup() {
+  Serial.begin(115200);  // Initialize serial
+
+  WiFi.mode(WIFI_STA); 
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+}
+//===============================================================
+//                        LOOP
+//===============================================================
+void loop() {
+  //----------------------------------------------------------------
+  // Connect or reconnect to WiFi
+  //----------------------------------------------------------------
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
+  //----------------------------------------------------------------    
+  // set the fields with the values
+  //----------------------------------------------------------------
+  ThingSpeak.setField(1,reading);
+  ThingSpeak.setField(2,BPM_actual);
+
+  //----------------------------------------------------------------
+  // figure out the status message
+  //----------------------------------------------------------------
+  if(reading>0){
+    myStatus=String("CONNECTED");
+  }
+  else{
+    myStatus=String("DISCONECTED")
+  }
+  // set the status
+  ThingSpeak.setStatus(myStatus);
+  
+  // write to the ThingSpeak channel
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(x == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+  //----------------------------------------------------------------
+  // Read Analog value and calculate BPM
+  //----------------------------------------------------------------
+  // Heart beat leading edge detected.
+      reading = analogRead(0);
+      if(reading > UpperThreshold && IgnoreReading == false){
+        if(FirstPulseDetected == false){
+          FirstPulseTime = millis();
+          FirstPulseDetected = true;
+        }
+        else{
+          SecondPulseTime = millis();
+          PulseInterval = SecondPulseTime - FirstPulseTime;
+          FirstPulseTime = SecondPulseTime;
+        }
+        IgnoreReading = true;
+      }
+
+      // Heart beat trailing edge detected.
+      if(reading < LowerThreshold){
+        IgnoreReading = false;
+      }  
+
+      BPM = (1.0/PulseInterval) * 60.0 * 1000;
+      if(bpm_prom<3){
+        bpm_prom=bpm_prom+1;
+        BPM_SUM = BPM + BPM_SUM
+      }
+      if(bpm_prom==3){
+        BPM_actual = BPM_SUM%3;
+        Serial.println(BPM_actual);
+      }
+       
+      Serial.print(reading);
+      Serial.print("\t");
+      Serial.print(PulseInterval);
+      Serial.print("\t");
+      Serial.print(BPM);
+      Serial.println(" BPM");
+      Serial.flush();
+      
+      // Please don't use delay() - this is just for testing purposes.
+      //delay(50);
+}
---
+++ medica_ecg_thingspealk_bpm/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "My Phone HTC"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "N1o2w3a4r5n6i7n8g9"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 921559			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "FU7NTQBS57BRW2VU"   // replace XYZ with your channel write API Key
